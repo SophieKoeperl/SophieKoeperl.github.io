4087472419215b484a7b39f2180d27acc5f949a3
sophie <sophie@sophies-mbp.eduroam.hs-furtwangen.de>
2018-03-22 13:37:04 +0100

    Aufgabe0

--- Aufgabe0/aufgabe0.ts
+++ Aufgabe0/aufgabe0.ts
@@ -1,8 +1,12 @@
+/* Aufgabe: 0 - Mini-Website Name: Sopehi Köperl Matrikel:257762 Datum: 22.03.2018 
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. */
+
 function Begrüßung() {
-  var name = prompt("Bitte gaben Sie Ihre Namen ein", "");
+  var name = prompt("Bitte geben Sie Ihren Namen ein", "");
   if (name != null) {
     document.getElementById("Type").innerHTML =
-      "Hallo" + name + "! Herzlich Willkommen auf der Mini-Website.";
+      "Hallo " + name + "! Herzlich Willkommen auf der Mini-Website.";
   }
 }
 
